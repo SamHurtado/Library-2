a731948cead7dde5ac8d50b5a84d08e3d4465b49
SamHurtado <samhurtado@icloud.com>
2025-10-16 14:13:43 -0400

    Update

---
+++ books.js
@@ -0,0 +1,130 @@
+function renderBooks() {
+    const booksWrapper = document.querySelector('.books');
+
+    const books = getBooks();
+
+    const booksHtml = books.map((book) => {
+    return `<div class="book">
+ <figure class="book__img--wrapper">
+    <img class="book__img" src="${book.url}" alt="">
+    </figure>
+<div class="book__title">
+    ${book.title}
+    </div>
+    <div class="book__ratings">
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star-half"></i>
+    </div>
+<div class="book__price">
+    <span class="book__price--normal">$${book.originalPrice}</span> $${book.salePrice}
+</div>
+    </div>`;
+    });
+    .join("");
+
+
+    booksWrapper.innerHTML = booksHtml;
+    console.log(booksHtml)
+    // booksWrapper.innerHTML = ;
+}
+
+setTimeout(() => {
+    renderBooks();
+});
+
+{/* // FAKE DATA */}
+function getBooks() {
+  return [
+    {
+      id: 1,
+      title: "Crack the Coding Interview",
+      url: "./Library JS/Library JS Starter/Library JS Starter/assets/crack the coding interview.png",
+      originalPrice: 49.95,
+      salePrice: 14.95,
+      rating: 4.5,
+    },
+    {
+      id: 2,
+      title: "Atomic Habits",
+      url: "./Library JS/Library JS Starter/Library JS Starter/assets/atomic habits.jpg",
+      originalPrice: 39,
+      salePrice: null,
+      rating: 5,
+    },
+    {
+      id: 3,
+      title: "Deep Work",
+      url: "./Library JS/Library JS Starter/Library JS Starter/assets/deep work.jpeg",
+      originalPrice: 29,
+      salePrice: 12,
+      rating: 5,
+    },
+    {
+      id: 4,
+      title: "The 10X Rule",
+      url: "./Library JS/Library JS Starter/Library JS Starter/assets/book-1.jpeg",
+      originalPrice: 44,
+      salePrice: 19,
+      rating: 4.5,
+    },
+    {
+      id: 5,
+      title: "Be Obsessed Or Be Average",
+      url: "./Library JS/Library JS Starter/Library JS Starter/assets/book-2.jpeg",
+      originalPrice: 32,
+      salePrice: 17,
+      rating: 4,
+    },
+    {
+      id: 6,
+      title: "Rich Dad Poor Dad",
+      url: "./Library JS/Library JS Starter/Library JS Starter/assets/book-3.jpeg",
+      originalPrice: 70,
+      salePrice: 12.5,
+      rating: 5,
+    },
+    {
+      id: 7,
+      title: "Cashflow Quadrant",
+      url: "./Library JS/Library JS Starter/Library JS Starter/assets/book-4.jpeg",
+      originalPrice: 11,
+      salePrice: 10,
+      rating: 4,
+    },
+    {
+      id: 8,
+      title: "48 Laws of Power",
+      url: "./Library JS/Library JS Starter/Library JS Starter/assets/book-5.jpeg",
+      originalPrice: 38,
+      salePrice: 17.95,
+      rating: 4.5,
+    },
+    {
+      id: 9,
+      title: "The 5 Second Rule",
+      url: "assets/book-6.jpeg",
+      originalPrice: 35,
+      salePrice: null,
+      rating: 4,
+    },
+    {
+      id: 10,
+      title: "Your Next Five Moves",
+      url: "./Library JS/Library JS Starter/Library JS Starter/assets/book-7.jpg",
+      originalPrice: 40,
+      salePrice: null,
+      rating: 4,
+    },
+    {
+      id: 11,
+      title: "Mastery",
+      url: "./Library JS/Library JS Starter/Library JS Starter/assets/book-8.jpeg",
+      originalPrice: 30,
+      salePrice: null,
+      rating: 4.5,
+    },
+  ];
+}
