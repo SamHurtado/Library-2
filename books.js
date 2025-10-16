e8dc044d23e2f4720f8bc9139cd8c75cb222fb8c
SamHurtado <samhurtado@icloud.com>
2025-10-16 15:14:52 -0400

    Update

--- books.js
+++ books.js
@@ -1,9 +1,28 @@
-function renderBooks() {
+function renderBooks(filter) {
     const booksWrapper = document.querySelector('.books');
 
     const books = getBooks();
 
-    const booksHtml = books.map((book) => {
+if (filter === 'LOW_TO_HIGH') {
+    books.sort((a, b) => a.originalPrice - b.originalPrice)
+}
+else if (filter === 'HIGH_TO_LOW') {
+    books.sort((a, b) => b.originalPrice - a.originalPrice)
+}
+else if (filter === 'RATING' ) {
+    books.sort((a, b) => b.rating - a.rating)
+}
+
+let ratingHTML = '';
+for (let i = 0; i < Math.floor(rating); ++i) {
+    ratingHTML += '<i class="fa-solid fa-star"></i>\n'
+}
+if (!Number.isInteger(rating)) {
+    ratingHTML += '<i class="fa-solid fa-star-half"></i>\n'
+}
+
+    const booksHtml = books
+    .map((book) => {
     return `<div class="book">
  <figure class="book__img--wrapper">
     <img class="book__img" src="${book.url}" alt="">
@@ -12,24 +31,35 @@ function renderBooks() {
     ${book.title}
     </div>
     <div class="book__ratings">
-    <i class="fa-solid fa-star"></i>
-    <i class="fa-solid fa-star"></i>
-    <i class="fa-solid fa-star"></i>
-    <i class="fa-solid fa-star"></i>
-    <i class="fa-solid fa-star-half"></i>
+    ${ratingHTML(book.rating)}
     </div>
 <div class="book__price">
-    <span class="book__price--normal">$${book.originalPrice}</span> $${book.salePrice}
+    // <span>$${book.originalPrice.toFixed(2)}</span>
 </div>
     </div>`;
-    });
+    })
     .join("");
 
 
     booksWrapper.innerHTML = booksHtml;
-    console.log(booksHtml)
-    // booksWrapper.innerHTML = ;
 }
+
+function ratingsHTML(rating) {
+    let ratingHTML = '';
+for (let i = 0; i < Math.floor(rating); ++i) {
+    ratingHTML += '<i class="fa-solid fa-star"></i>\n'
+}
+if (!Number.isInteger(rating)) {
+    ratingHTML += '<i class="fa-solid fa-star-half"></i>\n'
+}
+return ratingHTML;
+}
+
+function filterBooks(event) {
+        renderBooks(event.target.value)
+   
+}
+
 
 setTimeout(() => {
     renderBooks();
@@ -105,7 +135,7 @@ function getBooks() {
     {
       id: 9,
       title: "The 5 Second Rule",
-      url: "assets/book-6.jpeg",
+      url: "./Library JS/Library JS Starter/Library JS Starter/assets/book-6.jpeg",
       originalPrice: 35,
       salePrice: null,
       rating: 4,
