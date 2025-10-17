76ad46a0eca5f5ab5ad1b242f45e218d5b0ce445
SamHurtado <samhurtado@icloud.com>
2025-10-16 21:29:59 -0400

    Update

--- books.js
+++ books.js
@@ -1,25 +1,24 @@
-function renderBooks(filter) {
+let books;
+
+async function renderBooks(filter) {
     const booksWrapper = document.querySelector('.books');
 
-    const books = getBooks();
+   document.body.classList += ' books__loading'
+if (!books) {
+    books = await getBooks();
+}
+   booksWrapper.classList.remove('books__loading')
 
 if (filter === 'LOW_TO_HIGH') {
-    books.sort((a, b) => a.originalPrice - b.originalPrice)
+    books.sort((a, b) => (a. salePrice || a.originalPrice) - (b.salePrice || b.originalPrice))
 }
 else if (filter === 'HIGH_TO_LOW') {
-    books.sort((a, b) => b.originalPrice - a.originalPrice)
+    books.sort((a, b) => (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice))
 }
 else if (filter === 'RATING' ) {
     books.sort((a, b) => b.rating - a.rating)
 }
 
-// let ratingHTML = '';
-// for (let i = 0; i < Math.floor(rating); ++i) {
-//     ratingHTML += '<i class="fa-solid fa-star"></i>\n'
-// }
-// if (!Number.isInteger(rating)) {
-//     ratingHTML += '<i class="fa-solid fa-star-half"></i>\n'
-// }
 
     const booksHtml = books
     .map((book) => {
@@ -34,7 +33,7 @@ else if (filter === 'RATING' ) {
     ${ratingsHTML(book.rating)}
     </div>
 <div class="book__price">
-    // <span>$${book.originalPrice.toFixed(2)}</span>
+    ${priceHTML(book.originalPrice, book.salePrice)}
 </div>
     </div>`;
     })
@@ -42,6 +41,17 @@ else if (filter === 'RATING' ) {
 
 
     booksWrapper.innerHTML = booksHtml;
+}
+
+function priceHTML(originalPrice, salePrice) {
+    if (!salePrice) {
+        return `$${originalPrice.toFixed(2)}`
+    }
+    else {
+      return  `<span class="book__price--normal">$${originalPrice.toFixed(2)}</span> $${salePrice.toFixed(2)}`
+    }
+
+
 }
 
 function ratingsHTML(rating) {
@@ -65,10 +75,12 @@ setTimeout(() => {
     renderBooks();
 });
 
-{/* // FAKE DATA */}
+/* // FAKE DATA */
 function getBooks() {
-  return [
-    {
+   return new Promise ((resolve) => {
+        setTimeout(() => {
+           resolve() ([
+             {
       id: 1,
       title: "Crack the Coding Interview",
       url: "./Library JS/Library JS Starter/Library JS Starter/assets/crack the coding interview.png",
@@ -155,6 +167,8 @@ function getBooks() {
       originalPrice: 30,
       salePrice: null,
       rating: 4.5,
-    },
-  ];
+        },
+     ]);
+    }, 1000);
+  });
 }
